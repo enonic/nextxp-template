61699f0f94baae87e09a46b187dd18c315c79d4a
Espen Norderud <espen42@gmail.com>
2021-11-27 19:28:45 +0100

    Even clearer DefaultPart in the placeholder

--- src/components/pageeditor/_Part.tsx
+++ src/components/pageeditor/_Part.tsx
@@ -12,7 +12,7 @@ export type PartProps = {
 
 const DefaultPart = ({descriptor, config, path, content}:PartProps) => (
     <div id={`${path}-${descriptor}`} style={{marginTop:"2rem"}}>
-        <h6 style={{marginTop:"0", marginBottom:"0"}}>Part:</h6>
+        <h6 style={{marginTop:"0", marginBottom:"0"}}>DefaultPart:</h6>
         <h3 style={{marginTop:"0", marginBottom: "8px"}}>{descriptor}</h3>
         <h5 style={{marginTop:"0", marginBottom:"0"}}>config:</h5>
         <pre style={{width:"100%", whiteSpace:"pre-wrap", wordWrap: "break-word"}}>{JSON.stringify(config, null, 2)}</pre>
