862b00a765254621f38423bc8d92c5e220a08486
Espen Norderud <espen42@gmail.com>
2021-12-21 12:38:18 +0100

    Enforce null return from DataDump, or _Document.tsx will fail (#116)

--- src/xpAdapter/views/DataDump.tsx
+++ src/xpAdapter/views/DataDump.tsx
@@ -6,19 +6,20 @@ type Props = {
 }
 
 const DataDump = ({label, data}: Props) => (
-    data && (
-        <>
-            {label && (
-                <h5 style={{marginTop: "0", marginBottom: "0"}}>{label}:</h5>
-            )}
-            <pre style={{
-                fontSize: ".8em",
-                width: "100%",
-                whiteSpace: "pre-wrap",
-                wordWrap: "break-word"
-            }}>{JSON.stringify(data, null, 2)}</pre>
-        </>
-    )
+    (data && (
+            <>
+                {label && (
+                    <h5 style={{marginTop: "0", marginBottom: "0"}}>{label}:</h5>
+                )}
+                <pre style={{
+                    fontSize: ".8em",
+                    width: "100%",
+                    whiteSpace: "pre-wrap",
+                    wordWrap: "break-word"
+                }}>{JSON.stringify(data, null, 2)}</pre>
+            </>
+        )
+    ) || null
 );
 
 export default DataDump;
