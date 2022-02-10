8c22c3f9ca38924b6bf19bb55be347f3ddc0f5ca
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-02-10 18:26:44 +0300

    Add 3 column layout styling #164

--- src/components/layouts/ThreeColumnLayout.tsx
+++ src/components/layouts/ThreeColumnLayout.tsx
@@ -7,11 +7,28 @@ const ThreeColumnLayoutView = (props: LayoutProps) => {
     const {content, meta} = props;
 
     return (
-        <div className="row">
-            <RegionView className="col-sm-4" name="left" components={regions['left']?.components} content={content} meta={meta}/>
-            <RegionView className="col-sm-4" name="center" components={regions['center']?.components} content={content} meta={meta}/>
-            <RegionView className="col-sm-4" name="right" components={regions['right']?.components} content={content} meta={meta}/>
-        </div>
+        <>
+            <div className="row">
+                <RegionView className="col-sm-4" name="left" components={regions['left']?.components} content={content} meta={meta}/>
+                <RegionView className="col-sm-4" name="center" components={regions['center']?.components} content={content} meta={meta}/>
+                <RegionView className="col-sm-4" name="right" components={regions['right']?.components} content={content} meta={meta}/>
+            </div>
+            <style jsx>{`
+                .row {
+                    display: flex;
+                    gap: 10px;
+                    margin-bottom: 10px;
+                    flex-wrap: wrap;
+                }
+                .row :global(div[data-portal-region]) {
+                    flex: 1;
+                    min-width: 200px;
+                }
+                .row :global(.xp-page-editor-region-placeholder) {
+                    margin-bottom: 0;
+                }
+            `}</style>
+        </>
     );
 };
 
