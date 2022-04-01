d2d01b1c39ebddf6bc0f911299b8c20c113266e5
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-04-01 15:00:48 +0300

    418 response should only happen for proxy requests, otherwise 404 #201

--- src/pages/[[...contentPath]].tsx
+++ src/pages/[[...contentPath]].tsx
@@ -38,15 +38,15 @@ export const getServerSideProps: GetServerSideProps = async (context: Context): 
     // TODO: Drop? 404 handling should be sufficient for CS
     // HTTP 418 if CMS is not configured to render item yet
     // catch-all rendering is ignored for isRenderableRequest in edit mode to allow selecting descriptors in page editor
-    if (meta && (!meta.canRender || meta.catchAll && isEditRenderableRequest(context)) && meta.renderMode != RENDER_MODE.NEXT) {
-        context.res.statusCode = 418;
+    if (meta && (!meta.canRender || meta.catchAll && isRenderableRequestEditMode(context))) {
+        context.res.statusCode = meta.renderMode !== RENDER_MODE.NEXT ? 418 : 404;
     }
 
     let catchAllInNextProdMode = meta?.renderMode === RENDER_MODE.NEXT && !IS_DEV_MODE && meta?.catchAll;
 
     return {
         // HTTP 404
-        notFound: (error && error.code === '404') || catchAllInNextProdMode || undefined,
+        notFound: (error && error.code === '404') || context.res.statusCode === 404 || catchAllInNextProdMode || undefined,
         props: {
             common,
             data,
@@ -57,7 +57,7 @@ export const getServerSideProps: GetServerSideProps = async (context: Context): 
     }
 };
 
-function isEditRenderableRequest(context: Context): boolean {
+function isRenderableRequestEditMode(context: Context): boolean {
     const method = context.req.method;
     const mode = context.query['mode'];
     return method === 'HEAD' && mode === RENDER_MODE.EDIT;
