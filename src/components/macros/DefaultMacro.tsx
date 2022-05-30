6a499f59a0bfc66dca0af0851c48ba64d0199b5b
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-05-30 12:33:14 +0300

    Implement handling of edit mode in the base macro #277

--- src/components/macros/DefaultMacro.tsx
+++ src/components/macros/DefaultMacro.tsx
@@ -1,14 +1,9 @@
 import React from "react"
-import {RENDER_MODE} from '../../_enonicAdapter/utils';
 import {MacroProps} from '../../_enonicAdapter/views/BaseMacro';
 import HTMLReactParser from 'html-react-parser';
 
-const DefaultMacro = ({name, config, meta}: MacroProps) => {
-    if (meta?.renderMode === RENDER_MODE.EDIT) {
-        return <>{`[${name}]${config.body}[/${name}]`}</>
-    } else {
-        return HTMLReactParser(config.body) as JSX.Element;
-    }
-};
+const DefaultMacro = ({name, config, meta}: MacroProps) => (
+    HTMLReactParser(config.body) as JSX.Element
+);
 
 export default DefaultMacro;
