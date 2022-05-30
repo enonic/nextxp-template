3fc78aa649625a7749e909ae0945e870b30e370f
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-05-30 12:33:14 +0300

    Implement handling of edit mode in the base macro #277

--- src/_enonicAdapter/views/BaseMacro.tsx
+++ src/_enonicAdapter/views/BaseMacro.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {MacroConfig, MacroData, MetaData} from "../guillotine/getMetaData";
 import {ComponentRegistry} from '../ComponentRegistry';
+import {RENDER_MODE} from '../utils';
 
 const unescape = require('unescape');
 
@@ -18,13 +19,20 @@ export interface MacroProps {
 const BaseMacro = (props: BaseMacroProps) => {
     const {data, meta} = props;
 
+    const config = data.config[data.name] || {};
+    if (config?.body) {
+        config.body = unescape(config.body);
+    }
+    if (meta?.renderMode === RENDER_MODE.EDIT) {
+        const attrs = Object.entries(config)
+            .filter(entry => entry[0] !== 'body')
+            .map(entry => `${entry[0]}="${entry[1]}"`);
+        return `[${data.name}${attrs.length ? ` ${attrs.join(' ')}` : ''}${!config.body ? '/]' : `]${config.body}[/${data.name}]`}`
+    }
+
     const macro = ComponentRegistry.getMacro(data.descriptor);
     const MacroView = macro?.view;
     if (MacroView) {
-        const config = data.config[data.name];
-        if (config?.body) {
-            config.body = unescape(config.body);
-        }
         return <MacroView name={data.name} config={config} meta={meta}/>;
     } else if (data.descriptor) {
         console.warn(`BaseMacro: can not render macro '${data.descriptor}': no next view or catch-all defined`);
--- src/components/macros/YoutubeMacro.tsx
+++ src/components/macros/YoutubeMacro.tsx
@@ -1,13 +1,11 @@
 import React from "react"
-import {RENDER_MODE} from '../../_enonicAdapter/utils';
 import {MacroProps} from '../../_enonicAdapter/views/BaseMacro';
 
 const YoutubeMacro = ({name, config, meta}: MacroProps) => {
-    if (meta?.renderMode === RENDER_MODE.EDIT) {
-        return <>{`[${name} title="${config.title}" url="${config.url}"/]`}</>
-    } else {
-        return <>Youtube here</>
-    }
+    return <iframe width="560" height="315" src={config.url} title={config.title} frameBorder="0"
+                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+
+    </iframe>
 };
 
 export default YoutubeMacro;
