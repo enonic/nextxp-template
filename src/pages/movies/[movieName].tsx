31980e1241fd82d83420166611623d459b1bb0a8
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-07-04 18:46:38 +0300

    Make catch all router path statically generated with fallback: blocking for non-covered paths #309

--- src/pages/movies/[movieName].tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MainView from '../../_enonicAdapter/views/MainView';
-import {createStaticFunctions} from '../../_enonicAdapter/pages/static';
-
-export const {getStaticPaths, getStaticProps} = createStaticFunctions('movies', 'movieName');
-
-export default MainView;
