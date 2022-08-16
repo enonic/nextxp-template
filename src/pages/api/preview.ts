d8555b77b5ad04823f1dcae1cb492062f0f9ce70
Pavel Milkevich <pavel.milkevich@gmail.com>
2022-08-16 17:48:40 +0300

    Dynamically handle request to single component reload #369

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,5 +1,3 @@
-import {COMPONENT_SUBPATH_HEADER, FROM_XP_PARAM, RENDER_MODE_HEADER, XP_BASE_URL_HEADER} from '../../_enonicAdapter/utils';
-
 export default async function handler(req: any, res: any) {
     const {token: token, path} = req.query;
     if (token !== process.env.API_TOKEN) {
@@ -11,17 +9,12 @@ export default async function handler(req: any, res: any) {
         return res.status(400).json({message: 'Invalid path'})
     }
 
-    res.setPreviewData({
-        contentPath: path,
-        headers: {
-            [FROM_XP_PARAM]: req.headers[FROM_XP_PARAM],
-            [RENDER_MODE_HEADER]: req.headers[RENDER_MODE_HEADER],
-            [COMPONENT_SUBPATH_HEADER]: req.headers[COMPONENT_SUBPATH_HEADER],
-            [XP_BASE_URL_HEADER]: req.headers[XP_BASE_URL_HEADER],
-        }
-    }, {
-        maxAge: 10  // sec
-    });
+    console.info(`Previewing [${path}]...`);
+    // Enable Preview Mode by setting the cookies
+    // getStaticProps will be called in response to this request
+    // Do not use previewData because we cache preview token
+    // and data in lib-nextjs-proxy across requests
+    res.setPreviewData({});
 
     res.redirect(path);
 }
