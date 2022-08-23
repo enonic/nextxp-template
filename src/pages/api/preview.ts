393a9d8289d2b1942f50b73d9e978696c63f242c
Pavel Milkevich <pmi@enonic.com>
2022-08-23 11:06:04 +0200

    Pass headers as previewData from preview route #373

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,3 +1,10 @@
+import {
+    COMPONENT_SUBPATH_HEADER,
+    FROM_XP_PARAM,
+    RENDER_MODE_HEADER,
+    XP_BASE_URL_HEADER
+} from "../../_enonicAdapter/utils";
+
 export default async function handler(req: any, res: any) {
     const {token: token, path} = req.query;
     if (token !== process.env.API_TOKEN) {
@@ -14,7 +21,15 @@ export default async function handler(req: any, res: any) {
     // getStaticProps will be called in response to this request
     // Do not use previewData because we cache preview token
     // and data in lib-nextjs-proxy across requests
-    res.setPreviewData({});
+    res.setPreviewData({
+        contentPath: path,
+        headers: {
+            [FROM_XP_PARAM]: req.headers[FROM_XP_PARAM],
+            [RENDER_MODE_HEADER]: req.headers[RENDER_MODE_HEADER],
+            [COMPONENT_SUBPATH_HEADER]: req.headers[COMPONENT_SUBPATH_HEADER],
+            [XP_BASE_URL_HEADER]: req.headers[XP_BASE_URL_HEADER],
+        }
+    });
 
     res.redirect(path);
 }
