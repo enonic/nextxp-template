633adbe7bfddd7ba124de76ef93af8cfc3a15498
Pavel Milkevich <pmi@enonic.com>
2022-09-02 17:27:50 +0200

    Invalidate EVERYTHING on publish event #386

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,19 +1,38 @@
+import {recursiveFetchChildren} from "../[[...contentPath]]";
+
 export default async function handler(req: any, res: any) {
+    const {token, path, revalidateAll} = req.query;
     // Check for secret to confirm this is a valid request
-    if (req.query.token !== process.env.API_TOKEN) {
+    if (token !== process.env.API_TOKEN) {
         return res.status(401).json({message: 'Invalid token'});
     }
 
-    const path = req.query.path || '';
-    console.info(`Revalidating [${path}]...`);
     try {
-        await res.revalidate(path);
-        console.info(`Revalidating [${path}] done`);
+        if (revalidateAll) {
+            console.info('Started revalidating everything...');
+            const paths = await recursiveFetchChildren('\${site}/');
+            const promises = paths.map(item => revalidatePath(res, item.params.contentPath));
+            await Promise.all(promises);
+            console.info(`Done revalidating everything`);
+        } else {
+            await revalidatePath(res, path);
+            console.info(`Revalidated [${path}]`);
+        }
         return res.json({revalidated: true});
     } catch (err) {
         console.error(`Revalidating [${path}] error: ` + err);
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        return res.status(500).send({revalidated: false, error: err});
+        return res.status(500).json({revalidated: false, error: err});
     }
+}
+
+async function revalidatePath(res: any, path: string[] | string) {
+    let normalPath;
+    if (typeof path === 'string') {
+        normalPath = path.charAt(0) !== '/' ? '/' + path : path;
+    } else {
+        normalPath = '/' + path.join('/');
+    }
+    return res.revalidate(normalPath);
 }
