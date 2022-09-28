f349f3adde9b45fcd63d2b45dce1835984e06491
Pavel Milkevich <pmi@enonic.com>
2022-09-28 13:09:32 +0200

    Fixes for nextjs-demo #424

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 import {getUrl, RENDER_MODE, XP_REQUEST_TYPE} from '../_enonicAdapter/utils';
 import Header from '../components/views/Header';
 import Footer from '../components/views/Footer';
+import {FetchContentResult} from "../_enonicAdapter/guillotine/fetchContent";
 
 /**
  * Wraps all rendered components
  * @param Component Usually triggering [[...contentPath]].tsx, this component is BasePage.tsx
  * @param pageProps {{common, data, meta, error}}
  */
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({Component, pageProps}: AppProps<FetchContentResult>) {
 
     // Component rendering - for component updates in Content Studio without reloading page
     if (pageProps.meta) {
@@ -18,7 +19,7 @@ function MyApp({Component, pageProps}: AppProps) {
         if (meta.requestType === XP_REQUEST_TYPE.COMPONENT) {
             return <details data-single-component-output="true"><Component {...pageProps} /></details>;
         } else if (!meta.canRender
-                   || (meta.catchAll && meta.renderMode === RENDER_MODE.EDIT)) {
+            || (meta.catchAll && meta.renderMode === RENDER_MODE.EDIT)) {
             // return empty page, status is set in [[...contentPath.tsx]]
             return null;
         }
