enonic/nextxp-demo

Implement static incremental build POC in demo #289

src/_enonicAdapter/guillotine/getMetaData.ts
@@ -7,9 +7,10 @@ function sanitizeName(name: string): string {
 }
 
 export const macroConfigsQuery = (): string => {
-    return `config {
-        ${ComponentRegistry.getMacros().map(entry => `${sanitizeName(entry[0])}${entry[1].query}`).join('\n')}
-    }`
+    const macros = ComponentRegistry.getMacros();
+    return macros.length ? `config {
+        ${macros.map(entry => `${sanitizeName(entry[0])}${entry[1].query}`).join('\n')}
+    }` : '';
 }
 
 

enonic/nextxp-template

src/_enonicAdapter/guillotine/getMetaData.ts
@@ -8,9 +8,10 @@ function sanitizeMacroName(name: string): string {
 }
 
 export const macroConfigsQuery = (): string => {
-    return `config {
-        ${ComponentRegistry.getMacros().map(entry => `${sanitizeMacroName(entry[0])}${entry[1].query}`).join('\n')}
-    }`
+    const macros = ComponentRegistry.getMacros();
+    return macros.length ? `config {
+        ${macros.map(entry => `${sanitizeMacroName(entry[0])}${entry[1].query}`).join('\n')}
+    }` : '';
 }
 
 