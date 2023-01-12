0c117b374d56ea580384107a2f4b65091433af6f
Pavel Milkevich <pmi@enonic.com>
2023-01-12 16:17:26 +0100

    Use StaticContent tag to skip hydration in edit mode #510

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import type {AppProps} from 'next/app'
 import '../styles/globals.css'
-import React from 'react';
 import Header from '../components/views/Header';
 import Footer from '../components/views/Footer';
 import {FetchContentResult, RENDER_MODE, XP_REQUEST_TYPE} from '@enonic/nextjs-adapter';
 import {getUrl} from '@enonic/nextjs-adapter/UrlProcessor';
+import StaticContent from '../../../nextjs-adapter/views/StaticContent';
 
 /**
  * Wraps all rendered components
@@ -12,21 +12,26 @@ import {getUrl} from '@enonic/nextjs-adapter/UrlProcessor';
  * @param pageProps {{common, data, meta, error}}
  */
 function MyApp({Component, pageProps}: AppProps<FetchContentResult>) {
+    const isEdit = pageProps?.meta?.renderMode === RENDER_MODE.EDIT;
 
     // Component rendering - for component updates in Content Studio without reloading page
     if (pageProps.meta) {
         const meta = pageProps.meta;
         if (meta.requestType === XP_REQUEST_TYPE.COMPONENT) {
-            return <details data-single-component-output="true"><Component {...pageProps} /></details>;
+            return <StaticContent condition={isEdit}>
+                <details data-single-component-output="true">
+                    <Component {...pageProps} />
+                </details>
+            </StaticContent>;
         } else if (!meta.canRender
             || (meta.catchAll && meta.renderMode === RENDER_MODE.EDIT)) {
             // return empty page, status is set in [[...contentPath.tsx]]
             return null;
         }
     }
-    /*return <Component {...pageProps} />;*/
+
     return (
-        <>
+        <StaticContent condition={isEdit}>
             <Header
                 title="🔥 Next.XP"
                 logoUrl={getUrl('images/xp-shield.svg', pageProps.meta)}
@@ -40,7 +45,7 @@ function MyApp({Component, pageProps}: AppProps<FetchContentResult>) {
                 <Component {...pageProps} />
             </main>
             <Footer/>
-        </>
+        </StaticContent>
     );
 
 }
