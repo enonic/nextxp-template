b9b1691c55a1c9b8a38bef3f0a9ac28590929774
Pavel Milkevich <pmi@enonic.com>
2023-01-12 16:17:26 +0100

    Use StaticContent tag to skip hydration in edit mode #510
    (cherry picked from commit 0c117b374d56ea580384107a2f4b65091433af6f)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type {AppProps} from 'next/app'
 import '../styles/globals.css'
 import React from 'react';
 import {FetchContentResult, RENDER_MODE, XP_REQUEST_TYPE} from '@enonic/nextjs-adapter';
+import StaticContent from '../../../nextjs-adapter/views/StaticContent';
 
 /**
  * Wraps all rendered components
@@ -9,19 +10,28 @@ import {FetchContentResult, RENDER_MODE, XP_REQUEST_TYPE} from '@enonic/nextjs-a
  * @param pageProps {{common, data, meta, error}}
  */
 function MyApp({Component, pageProps}: AppProps<FetchContentResult>) {
+    const isEdit = pageProps?.meta?.renderMode === RENDER_MODE.EDIT;
 
     // Component rendering - for component updates in Content Studio without reloading page
     if (pageProps.meta) {
         const meta = pageProps.meta;
         if (meta.requestType === XP_REQUEST_TYPE.COMPONENT) {
-            return <details data-single-component-output="true"><Component {...pageProps} /></details>;
+            return <StaticContent condition={isEdit}>
+                <details data-single-component-output="true">
+                    <Component {...pageProps} />
+                </details>
+            </StaticContent>;
         } else if (!meta.canRender
             || (meta.catchAll && meta.renderMode === RENDER_MODE.EDIT)) {
             // return empty page, status is set in [[...contentPath.tsx]]
             return null;
         }
     }
-    return <Component {...pageProps} />;
+    return (
+        <StaticContent condition={isEdit}>
+            <Component {...pageProps} />
+        </StaticContent>
+    );
 
 }
 
