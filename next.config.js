08af8e7f18e0d794bc21c7696625b7fdffa085be
Pavel Milkevich <pmi@enonic.com>
2023-03-03 11:09:50 +0100

    Update to use new server startup routine and config #541

--- next.config.js
+++ next.config.js
@@ -1,23 +1,34 @@
 const withTM = require('next-transpile-modules')(['@enonic/nextjs-adapter']);
+const withEnonicCache = require('@enonic/nextjs-adapter/server').withEnonicCache;
 
-// neededed to eval inline react scripts
-const cspHeaders = [
-    {
-        key: 'Content-Security-Policy',
-        value: `script-src 'self' 'unsafe-eval' 'unsafe-inline';`
+function getEnonicWebpackConfig(config) {
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        // client-side resolution for node modules
+        fs: false
     }
-]
+    return config;
+}
 
-module.exports = withTM({
+async function getEnonicHeaders() {
+    return [
+        {
+            // Apply these headers to all routes in your application.
+            source: '/:path*',
+            headers: [
+                {
+                    key: 'Content-Security-Policy',
+                    value: `script-src 'self' 'unsafe-eval' 'unsafe-inline';`
+                }
+            ],
+        },
+    ]
+}
+
+const config = {
     reactStrictMode: true,
+    webpack: getEnonicWebpackConfig,
+    headers: getEnonicHeaders,
+};
 
-    async headers() {
-        return [
-            {
-                // Apply these headers to all routes in your application.
-                source: '/:path*',
-                headers: cspHeaders,
-            },
-        ]
-    },
-});
+module.exports = withTM(withEnonicCache(config));
