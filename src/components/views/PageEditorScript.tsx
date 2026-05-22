49763c4ae6ef22238397d1a4c602fb6e27314cd6
Pavel Milkevich <pmi@enonic.com>
2026-05-22 10:49:14 +0200

    Migrate changes from next-demo to next-template #261
    - bump page-editor
    - bump nextjs-adapter

--- src/components/views/PageEditorScript.tsx
+++ src/components/views/PageEditorScript.tsx
@@ -1,26 +1,23 @@
 'use client';
 
 import {RENDER_MODE} from '@enonic/nextjs-adapter';
-
-import {useLayoutEffect} from 'react';
+import {EditorEvents, PageEditor} from '@enonic/page-editor';
 
 const PageEditorScript = function ({mode}: { mode: RENDER_MODE }) {
-    useLayoutEffect(() => {
-        // TODO: change to static import after jquery is removed from page-editor (ssr fails now)
-        // because of dynamic import it gets init too late and misses the event from CS
-        import('@enonic/page-editor').then(({PageEditor, EditorEvents}) => {
-            if (!PageEditor.isInitialized()) {
-                PageEditor.init(mode === RENDER_MODE.EDIT);
-                console.info(`Page editor started in ${mode} mode.`);
-            }
 
-            PageEditor.on(EditorEvents.ComponentLoadRequest, (event => {
-                    const data = event.getData();
-                    console.log('Component load request', data);
-                })
-            );
+    if (PageEditor.isInitialized()) {
+        return;
+    }
+
+    PageEditor.init({editMode: mode === RENDER_MODE.EDIT});
+    console.info(`Page editor started in ${mode} mode.`);
+
+    PageEditor.on(EditorEvents.ComponentLoadRequest, (event => {
+            const data = event.getData();
+            console.log('Component load request', data);
         })
-    })
+    );
+
     return null;
 };
 
